3a061faa455aa9b496f42e30074a6b4953fccdf5
Arturo Murillo <arturomurillo@MacBook-Pro.local>
2024-05-27 11:14:10 -0600

    feat(workflows): enable workflow

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,27 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'build',
+          'chore',
+          'ci',
+          'docs',
+          'improvement',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'test',
+        ],
+      ],
+      'header-max-length': [2, 'always', 100],
+      'scope-case': [2, 'always', 'lower-case'],
+      'scope-empty': [2, 'never'],
+    },
+  };
+
